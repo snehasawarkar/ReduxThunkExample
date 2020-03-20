99de977d3921baf42b7095f2a41f572640a1c9ee
311119 <sneha.sawarkar@wu.com>
2020-03-20 19:26:39 +0530

    form controls added

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CustomerList({ customers, addCustomer }){
-    const handleClick = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [id, setId] = useState("");
+
+    const handleChange = (setState, event) => {
+        setState(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
         addCustomer(
             { 
-                id: 2,
-                firstName: "Umar",
-                lastName: "Siddiqui"
+                id: id,
+                firstName: firstName,
+                lastName: lastName
             }
         )
+
+        event.preventDefault();
     }
     return (
         <div>
             {
-                customers.map(customer => <div> {customer.firstName} </div>)
+                customers.map(customer => <div>{customer.id} {customer.firstName} {customer.lastName} </div>)
             }
-            <button onClick={handleClick}>Add customer</button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    Id:
+                    <input onChange={handleChange.bind(null, setId)} type="text" name="id" />
+                </div>
+                <div>
+                    First Name:
+                    <input onChange={handleChange.bind(null, setFirstName)} type="text" name="firstname" />
+                </div>
+                <div>
+                    Last Name:
+                    <input onChange={handleChange.bind(null, setLastName)} type="text" name="lastname" />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+            {/* <button onClick={handleClick}>Add customer</button> */}
         </div>
     )
 }
